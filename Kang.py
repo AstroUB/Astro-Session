844c0a8a9ca470e7ff356bf2527a8ab06646af79
ʟ๏ᴠᴇʀ ◠‿◕ <79841865+loverboyXD@users.noreply.github.com>
2021-09-01 11:03:39 +0530

    ~Little Help for Kangers~😆

---
+++ Kang.py
@@ -0,0 +1,9 @@
+STRING = [
+    "ABEEE", 
+    "Madarchod",
+    "Randi Chod ke",
+    "Bina Lode ke randwe",
+    "apne BAAP KA REPO KANG MAAT KR",
+    "WARNA CONDOM LAGA KR PREGNANT KR DUNGA",
+  ]
+print(STRING)
